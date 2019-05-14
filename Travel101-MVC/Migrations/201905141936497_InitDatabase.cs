6961e439e06ec58002ba4c545b3f2de81b4d5081
deathsmore <anhtuann0210@gmail.com>
2019-05-15 02:43:00 +0700

    add dbcontext, enable migration and init database

---
+++ Travel101-MVC/Migrations/201905141936497_InitDatabase.cs
@@ -0,0 +1,16 @@
+namespace Travel101_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitDatabase : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
